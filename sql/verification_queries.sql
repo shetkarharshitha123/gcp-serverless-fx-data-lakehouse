699c1c9f1199dd38a492ee526d4c3ca61cb72066
Shetkar Harshitha Patil <shetkarharsha49@gmail.com>
2026-09-11 18:04:40 +0530

    Add SQL query for currency rates verification

--- sql/verification_queries.sql
+++ sql/verification_queries.sql
@@ -1,1 +1,7 @@
+SELECT 
+  rate_date,
+  COUNT(*) AS curated_records_count,
+  ROUND(AVG(exchange_rate), 4) AS avg_exchange_rate,
+  MIN(ingested_at) AS first_ingested
+FROM ` snappy-mapper-498509-e0.analytics_lakehouse.currency_rates_gold`GROUP BY rate_dateORDER BY rate_date DESC;
 
