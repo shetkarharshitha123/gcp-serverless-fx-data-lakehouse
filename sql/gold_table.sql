c4126f1725c00ce78a07c6cd99086432422c0dc5
Shetkar Harshitha Patil <shetkarharsha49@gmail.com>
2026-09-11 18:03:28 +0530

    Create currency_rates_gold table with partitioning

--- sql/gold_table.sql
+++ sql/gold_table.sql
@@ -1,1 +1,9 @@
+CREATE TABLE IF NOT EXISTS `snappy-mapper-498509-e0.analytics_lakehouse.currency_rates_gold`
+(
+  base_currency STRING,
+  target_currency STRING,
+  exchange_rate NUMERIC,
+  rate_date DATE,
+  ingested_at TIMESTAMP)PARTITION BY rate_date
+CLUSTER BY target_currency, base_currency;
 
